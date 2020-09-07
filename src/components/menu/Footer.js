8a0eec9a4fe0a0191ff00f36a259920d7ef09068
MZubairLohar <zubairgituser@gmail.com>
2020-09-07 16:21:28 +0500

    complete project

---
+++ src/components/menu/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../../assets/css/footer.css';
+
+const Footer = () => (
+  <div className="footer page-footer font-small blue">
+    <div className="custom-footer text-center py-3">© :
+      <a className="custom-link" href="https://www.corecis.org/"> Corecis </a>
+    </div>
+  </div>
+);
+
+export default Footer;
